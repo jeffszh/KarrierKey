ba6c9fcb2f3c5c20fb58fd8166d867df60018b32
Jeff <zhsu@amwares.com>
2020-08-13 18:16:50 +0800

    如何响应消息呢？困难重重啊。

--- src/main/kotlin/cn/jeff/app/karrier_key/MainWnd.kt
+++ src/main/kotlin/cn/jeff/app/karrier_key/MainWnd.kt
@@ -1,16 +1,20 @@
 package cn.jeff.app.karrier_key
 
-import com.sun.jna.platform.win32.User32
+import com.sun.jna.platform.win32.*
 import javafx.fxml.FXMLLoader
 import javafx.scene.layout.BorderPane
 import tornadofx.*
+import java.awt.event.KeyEvent
 
 class MainWnd : View("试试WinAPI") {
 
 	override val root: BorderPane
 	private val j: MainWndJ
 	private val user = User32.INSTANCE
-//	private val kernel = Kernel32.INSTANCE
+	private val kernel = Kernel32.INSTANCE
+	private val hWnd: WinDef.HWND
+	private val atom01 = 1001
+	private val atom02 = 1002
 
 	init {
 		val loader = FXMLLoader()
@@ -19,6 +23,24 @@ class MainWnd : View("试试WinAPI") {
 		))
 		j = loader.getController()
 		j.k = this
+
+		hWnd = user.FindWindow(null, "试试WinAPI")
+		registerKeys()
+
+		primaryStage.setOnCloseRequest {
+			println("窗口关闭：$it")
+			unregisterKeys()
+		}
+	}
+
+	private fun registerKeys() {
+		user.RegisterHotKey(hWnd, atom01, WinUser.MOD_CONTROL, KeyEvent.VK_SUBTRACT)
+		user.RegisterHotKey(hWnd, atom01, WinUser.MOD_CONTROL, KeyEvent.VK_ADD)
+	}
+
+	private fun unregisterKeys() {
+		user.UnregisterHotKey(hWnd.pointer, atom01)
+		user.UnregisterHotKey(hWnd.pointer, atom01)
 	}
 
 	fun btn01Click() {
