a9f773dc39a9ed64d596eedcff950b37f3963408
Jeff <zhsu@amwares.com>
2020-08-13 17:40:55 +0800

    搭好基本框架。

--- src/main/kotlin/cn/jeff/app/karrier_key/MainWnd.kt
+++ src/main/kotlin/cn/jeff/app/karrier_key/MainWnd.kt
@@ -1,8 +1,29 @@
 package cn.jeff.app.karrier_key
 
+import com.sun.jna.platform.win32.User32
+import javafx.fxml.FXMLLoader
 import javafx.scene.layout.BorderPane
 import tornadofx.*
 
-class MainWnd : View("My View") {
-	override val root: BorderPane by fxml()
+class MainWnd : View("试试WinAPI") {
+
+	override val root: BorderPane
+	private val j: MainWndJ
+	private val user = User32.INSTANCE
+//	private val kernel = Kernel32.INSTANCE
+
+	init {
+		val loader = FXMLLoader()
+		root = loader.load(javaClass.getResourceAsStream(
+				"/cn/jeff/app/karrier_key/MainWnd.fxml"
+		))
+		j = loader.getController()
+		j.k = this
+	}
+
+	fun btn01Click() {
+		val hWnd = user.FindWindow(null, "试试WinAPI")
+		information("hWnd = $hWnd")
+	}
+
 }
---
+++ src/main/kotlin/cn/jeff/app/karrier_key/MainWndJ.java
@@ -0,0 +1,11 @@
+package cn.jeff.app.karrier_key;
+
+public class MainWndJ {
+
+	public MainWnd k;
+
+	public void btn01Click() {
+		k.btn01Click();
+	}
+
+}
